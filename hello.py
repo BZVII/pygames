0d78dfad34098569d50f81b990de8ed1a62f7d0e
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-04-27 20:59:01 +0200

    Fin hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 import pygame as pg
+import sys
+
+def fin_juego():
+    pg.quit()
+    sys.exit()
 
 pg.init()
 pantalla = pg.display.set_mode((600, 400))
@@ -9,7 +14,8 @@ game_over = False
 while not game_over:
     # Gestion de eventos
     for evento in pg.event.get(): 
-        pass
+        if evento.type == pg.QUIT:
+            game_over = True
 
 
     # Gestion del estado
@@ -18,3 +24,6 @@ while not game_over:
     # Refrescar pantalla    
     pantalla.fill((0, 255, 0))
     pg.display.flip()
+
+
+fin_juego()
