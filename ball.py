9cef4cee3e5142b85426b00c8595084e5f2b13ac
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-04-30 00:39:34 +0200

    Tres vidas

--- ball.py
+++ ball.py
@@ -24,46 +24,82 @@ class Bola():
         self.vx = vx
         self.vy = vy
         self.color = color
-        self.radio = radio
+        self.anchura = radio*2
+        self.altura = radio*2
 
     def actualizar(self):
         self.x += self.vx
         self.y += self.vy
 
-        if self.y <=0 or self.y >=ALTO:
+        if self.y <=0:
             self.vy = -self.vy
         
         if self.x <=0 or self.x >=ANCHO:
             self.vx = -self.vx
 
+        if self.y >=ALTO:
+            self.x = ANCHO // 2
+            self.y = ALTO // 2
+            self.vx = randint(5,10)*choice([-1, 1])
+            self.vy = randint(5,10)*choice([-1, 1])
+            return True
+        return False
+
     def dibujar(self, lienzo):
-        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.radio)
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.anchura//2)
+
+    def comprueba_colision(self, objeto):
+        '''
+        if self.x >= objeto.x and self.x <= objeto.x+objeto.anchura or \
+           self.x+self.anchura >= objeto.x and self.x+self.anchura <= objeto.x + objeto.anchura:
+            choqueX = True
+        else:
+            choqueX = False
+        '''
+
+        choqueX = self.x >= objeto.x and self.x <= objeto.x+objeto.anchura or \
+           self.x+self.anchura >= objeto.x and self.x+self.anchura <= objeto.x + objeto.anchura
+        choqueY = self.y >= objeto.y and self.y <= objeto.y+objeto.altura or \
+           self.y+self.altura >= objeto.y and self.y+self.altura <= objeto.y + objeto.altura
+
+        if choqueX and choqueY:
+            self.vy *= -1
 
 
 class Raqueta():
     def __init__(self, x=0, y=0):
         self.altura = 10
-        self.anchura = 250
+        self.anchura = 100
         self.color = (255, 255, 255)
-        self.x = (ANCHO + self.anchura) // 2
-        self.y = ALTO - self.altura
+        self.x = (ANCHO - self.anchura) // 2
+        self.y = ALTO - self.altura - 15
+        self.vy = 0
+        self.vx = 13
 
     def dibujar(self, lienzo):
         rect = pg.Rect(self.x, self.y, self.anchura, self.altura)
         pg.draw.rect(lienzo, self.color, rect)
 
+    def actualizar(self):
+        teclas_pulsadas = pg.key.get_pressed() 
+
+        if teclas_pulsadas[pg.K_LEFT] and self.x > 0:
+            self.x -= self.vx
+        if teclas_pulsadas[pg.K_RIGHT] and self.x < ANCHO - self.anchura:
+            self.x += self.vx
 
 
+vidas = 3
 bola = Bola(randint(0, ANCHO),
             randint(0, ALTO),
             randint(5, 10)*choice([-1, 1]),
             randint(5, 10)*choice([-1, 1]),
-            (randint(0, 255), randint(0,255), randint(0,255)))
+            AZUL)
 
 raqueta = Raqueta()
 
 game_over = False
-while not game_over:
+while not game_over and vidas > 0:
     v = reloj.tick(60)
     #Gestion de eventos
     for evento in pg.event.get():
@@ -71,7 +107,11 @@ while not game_over:
             game_over = True
 
     # Modificación de estado
-    bola.actualizar()
+    raqueta.actualizar()
+    pierdebola = bola.actualizar()
+    if pierdebola:
+        vidas -= 1
+    bola.comprueba_colision(raqueta)
 
     # Gestión de la pantalla
     pantalla.fill(NEGRO)
@@ -80,6 +120,8 @@ while not game_over:
 
 
     pg.display.flip()
+    if pierdebola:
+        pg.time.delay(500)
 
 pg.quit()
 sys.exit()
