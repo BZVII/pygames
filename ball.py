cda09238a9df95062b5623a946d18f436958bf6d
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-04-29 22:09:30 +0200

    Fin de bolas sueltas, a por la raqueta

--- ball.py
+++ ball.py
@@ -1,18 +1,9 @@
 import pygame as pg
 import sys
-from random import randint
+from random import randint, choice
 
-def rebotaX(x):
-    if x <=0 or x >=ANCHO:
-        return -1
 
-    return 1
 
-def rebotaY(y):
-    if y <=0 or y >=ALTO:
-        return -1
-
-    return 1
 
 
 ROJO = (255, 0, 0)
@@ -27,12 +18,30 @@ pantalla = pg.display.set_mode((ANCHO, ALTO))
 reloj = pg.time.Clock()
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color):
+    def __init__(self, x, y, vx=5, vy=5, color= (255, 255, 255), radio=10):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.color = color
+        self.radio = radio
+
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.y <=0 or self.y >=ALTO:
+            self.vy = -self.vy
+        
+        if self.x <=0 or self.x >=ANCHO:
+            self.vx = -self.vx
+
+    '''
+        bola.dibujar(pantalla)
+        #pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
+    '''
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.radio)
 
 
 
@@ -40,8 +49,8 @@ bolas = []
 for _ in range(10):
     bola = Bola(randint(0, ANCHO),
                 randint(0, ALTO),
-                randint(5, 10),
-                randint(5, 10),
+                randint(5, 10)*choice([-1, 1]),
+                randint(5, 10)*choice([-1, 1]),
                 (randint(0, 255), randint(0,255), randint(0,255)))
 
     bolas.append(bola)
@@ -56,17 +65,13 @@ while not game_over:
 
     # Modificación de estado
     for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
-
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
-
+        bola.actualizar()
 
     # Gestión de la pantalla
     pantalla.fill(NEGRO)
     for bola in bolas:
-        pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
+        bola.dibujar(pantalla)
+        #pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10)
 
 
     pg.display.flip()
