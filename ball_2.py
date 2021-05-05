faac7d50f5b3d194d3d4a42d1ed3aab33ba2186d
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-05-06 00:33:36 +0200

    Estados bola

--- ball_2.py
+++ ball_2.py
@@ -61,38 +61,59 @@ class Raqueta(pg.sprite.Sprite):
             
 
 class Bola(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y,):
         super().__init__()
         self.image = pg.image.load('./images/ball1.png').convert_alpha()
         self.rect = self.image.get_rect(center=(x,y))
+        self.xOriginal = x
+        self.yOriginal = y
+        self.estoyViva = True
 
         self.vx = random.randint(5, 10) * random.choice([-1, 1])
         self.vy = random.randint(5, 10) * random.choice([-1, 1])
 
+    def prueba_colision(self, grupo):
+        candidatos = pg.sprite.spritecollide(self, grupo, False)
+        if len(candidatos) > 0:
+            self.vy *= -1
 
     def update(self, dt):
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+        if self.estoyViva:
+            self.rect.x += self.vx
+            self.rect.y += self.vy
 
-        if self.rect.left <= 0 or self.rect.right >= ANCHO:
-            self.vx *= -1 
-        if self.rect.top <= 0 or self.rect.bottom >= ALTO:
-            self.vy *= -1
+            if self.rect.left <= 0 or self.rect.right >= ANCHO:
+                self.vx *= -1 
+            if self.rect.top <= 0:
+                self.vy *= -1
+
+            if self.rect.bottom >= ALTO:
+                self.estoyViva = False
+        else:
+            self.rect.center = (self.xOriginal, self.yOriginal)
+            self.vx = random.randint(5, 10) * random.choice([-1, 1])
+            self.vy = random.randint(5, 10) * random.choice([-1, 1])
+            self.estoyViva = True
+
 
 class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
-        self.botes = 0
+        self.vidas = 3
         self.todoGrupo = pg.sprite.Group()
+        self.grupoJugador = pg.sprite.Group()
+        self.grupoLadrillos = pg.sprite.Group()
 
         self.cuentaSegundos = Marcador(10,10)
         self.todoGrupo.add(self.cuentaSegundos)
 
-        self.bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
+        self.bola = Bola(ANCHO // 2, ALTO // 2)
         self.todoGrupo.add(self.bola)    
 
         self.raqueta = Raqueta(x = ANCHO//2, y = ALTO - 40)
-        self.todoGrupo.add(self.raqueta)
+        self.grupoJugador.add(self.raqueta)
+
+        self.todoGrupo.add(self.grupoJugador, self.grupoLadrillos)
 
 
     def bucle_principal(self):
@@ -100,7 +121,7 @@ class Game():
         reloj = pg.time.Clock()
         contador_milisegundos = 0
         segundero = 0
-        while not game_over: 
+        while not game_over and self.vidas > 0: 
             dt = reloj.tick(FPS)
             contador_milisegundos += dt
 
@@ -113,7 +134,10 @@ class Game():
                     game_over = True
 
             self.cuentaSegundos.text = segundero
+            self.bola.prueba_colision(self.grupoJugador)
             self.todoGrupo.update(dt)
+            if not self.bola.estoyViva:
+                self.vidas -= 1
 
             self.pantalla.fill((0,0,0))
             self.todoGrupo.draw(self.pantalla)
