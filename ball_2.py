c77b2cff997204b7941b201bcdbf716a3e9d3622
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-05-05 22:30:42 +0200

    Raqueta rectangulo rojo

--- ball_2.py
+++ ball_2.py
@@ -18,7 +18,27 @@ class Marcador(pg.sprite.Sprite):
     def update(self):
         self.image = self.fuente.render(str(self.text), True, self.color)
 
+class Raqueta(pg.sprite.Sprite):
+    def __init__(self, x, y, w=100, h=30):
+        super().__init__()
+        self.image = pg.Surface((w, h), pg.SRCALPHA, 32)
+        pg.draw.rect(self.image, (255,0,0), pg.Rect(0,0,w,h))
+        self.rect = self.image.get_rect(centerx = x, bottom = y)
+        self.vx = 7
 
+    def update(self):
+        teclas_pulsadas = pg.key.get_pressed()
+        if teclas_pulsadas[pg.K_LEFT]:
+            self.rect.x -= self.vx
+
+        if teclas_pulsadas[pg.K_RIGHT]:
+            self.rect.x += self.vx
+
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= ANCHO:
+            self.rect.right = ANCHO
+            
 
 class Bola(pg.sprite.Sprite):
     def __init__(self, x, y):
@@ -43,13 +63,16 @@ class Game():
     def __init__(self):
         self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         self.botes = 0
-        self.cuentaSegundos = Marcador(10,10)
-
         self.todoGrupo = pg.sprite.Group()
-        for i in range(random.randint(1, 20)):
-            bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
-            self.todoGrupo.add(bola)
+
+        self.cuentaSegundos = Marcador(10,10)
         self.todoGrupo.add(self.cuentaSegundos)
+
+        self.bola = Bola(random.randint(0, ANCHO), random.randint(0, ALTO))
+        self.todoGrupo.add(self.bola)    
+
+        self.raqueta = Raqueta(x = ANCHO//2, y = ALTO - 40)
+        self.todoGrupo.add(self.raqueta)
 
 
     def bucle_principal(self):
@@ -77,6 +100,8 @@ class Game():
 
 
             pg.display.flip()
+
+
 
 if __name__ == '__main__':
     pg.init()
